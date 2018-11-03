de154889020235ee9a9e93568db227dd81207872
Max Ranieri <otherranieri@gmail.com>
2018-11-03 15:10:16 -0400

    Fix name

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,51 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <header>
+      <h1>loglog</h1>
+    </header>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'loglog'
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+h1 {
+  margin-top: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+header {
+  background: #87a9cc;
+}
+
+header h1 {
+  font-family: 'Pacifico', cursive;
+  color: #c09569;
+  text-shadow:
+   0 1px 0px #000,
+   -1px 0 0px #000,
+   -1px 2px 0px #000,
+   -2px 1px 0px #000,
+   -2px 3px 0px #000,
+   -3px 2px 0px #000,
+   -3px 4px 0px #000,
+   -4px 3px 0px #000,
+   -4px 5px 0px #000,
+   -5px 4px 0px #000;
 }
 </style>
