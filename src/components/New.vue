da8252fc8ea227c4c9523a352686a222360e1b8c
Max Ranieri <otherranieri@gmail.com>
2018-11-03 19:44:18 -0400

    Modal on new poop page

--- src/components/New.vue
+++ src/components/New.vue
@@ -18,20 +18,15 @@
         <option value="6">Type 5 - Mushy, ragged</option>
         <option value="7">Type 7 - Liquid</option>
       </select>
-      <div class="type_ref">
-        <a href="#">see type descriptions</a>
+      <div class="type_ref" id="modal_btn">
+        <a href="#">image reference</a>
       </div>
 
-      <div class="type_popup">
-        <table>
-          <tbody>
-            <tr>
-              <td>Type 1</td>
-              <td></td>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
+      <div class="type_popup modal" id="type_modal">
+        <div class="modal-content">
+          <span class="close">&times;</span>
+          <img src="../assets/bristol_stool_scale.png" />
+        </div>
       </div>
     </div>
     <button class="send">submit</button>
@@ -56,6 +51,33 @@ export default {
       if (cur_len < 20) remaining.style.color = 'red'
       else remaining.style.color = 'black'
     })
+
+    // Get the modal
+    var modal = document.getElementById('type_modal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("modal_btn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
   }
 }
 </script>
@@ -117,5 +139,43 @@ textarea {
 form {
   padding: 10px;
 }
+
+/* modal */
+.modal {
+    display: none; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+.modal-content {
+    background-color: #fefefe;
+    margin: 5% auto; /* 15% from the top and centered */
+    padding: 20px;
+    border: 1px solid #888;
+    width: 80%; /* Could be more or less, depending on screen size */
+}
+
+.close {
+    color: #aaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+}
+
+
 
 </style>
